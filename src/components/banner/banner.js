54c80621af9a9d5779e7c1f936ff05411a426377
pbrito1 <pedrodebrito2202@gmail.com>
2025-07-12 00:06:58 -0300

    initialize a project using react

---
+++ src/components/banner/banner.js
@@ -0,0 +1,11 @@
+import './banner.css'
+
+function Banner() {
+    return(
+        <header className="banner-image">
+            <img src='/banner.png' alt="Banner" />
+        </header>
+    )
+}
+
+export default Banner;
